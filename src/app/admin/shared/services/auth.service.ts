4f4515378fadbc56ae399ad705b2f57a6587879b
Aleksandr Korol <korol1368@ukr.net>
2020-12-11 22:16:34 +0200

    139. Настройка firebase

--- src/app/admin/shared/services/auth.service.ts
+++ src/app/admin/shared/services/auth.service.ts
@@ -2,6 +2,9 @@ import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {User} from '../../../shared/interfaces/user.interface';
 import {Observable} from 'rxjs';
+import {environment} from '../../../../environments/environment';
+import {tap} from 'rxjs/operators';
+import {FbAuthResponse} from '../../../shared/interfaces/fb-auth-response.interface';
 
 @Injectable()
 
@@ -14,7 +17,10 @@ export class AuthService {
   }
 
   login(user: User): Observable<any> {
-    return this.http.post('', user);
+    return this.http.post(`https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${environment.apiKey}`, user)
+      .pipe(
+        tap(this.setToken)
+      );
   }
 
   logout(): void {
@@ -23,5 +29,9 @@ export class AuthService {
 
   isAuthenticated(): boolean {
     return !!this.token;
+  }
+
+  private setToken(response: FbAuthResponse): void {
+    console.log (response);
   }
 }
